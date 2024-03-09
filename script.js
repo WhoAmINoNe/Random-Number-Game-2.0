4de77dcbe79e84bf65076c1c7052bbe444171eb1
WhoAmINoNe <161349882+WhoAmINoNe@users.noreply.github.com>
2024-03-09 17:17:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+let number = document.querySelectorAll(".getNumber")
+let display = document.querySelector(".displayInput")
+let back = document.querySelector("#back")
+let ok = document.querySelector("#ok")
+let modal = document.querySelector(".modal")
+let remBlur = document.querySelector(".addBlur")
+let IamAgree = document.querySelector("#IamAgree")
+
+let numberOfAttempts = null
+let maxOfNumber = null
+let numberOfAttemptsValue = null
+let random = null
+
+let count = 1
+
+const alertFun = (alertInnerHTML, type, load) => {
+    const alert = document.querySelector(".alert")
+    const span = document.querySelector(".span")
+
+    span.innerHTML = alertInnerHTML
+
+    span.classList.add("d-block", "show", type)
+
+    setTimeout(() => {
+        alert.classList.remove("d-block", "show", type)
+
+        load ? window.location.reload() : ""
+
+        span.innerHTML = ""
+    }, 3000)
+}
+
+const thinkRandomNumber = (max) => Math.floor(Math.random() * max)
+
+for (let i of number) {
+    i.addEventListener("click", () => {
+        if (display.innerText.length < 3) {
+            display.innerText += i.textContent.trim()
+        }
+    })
+}
+
+back.addEventListener("click", () => {
+    display.innerText = display.innerText.slice(0, -1)
+})
+
+IamAgree.addEventListener("click", () => {
+    modal.classList.remove("show", "d-block")
+    remBlur.classList.remove("blur")
+
+    numberOfAttempts = document.querySelector("#numberOfAttempt").value
+    maxOfNumber = document.querySelector("#maxOfNumber").value
+    numberOfAttemptsValue = numberOfAttempts
+    
+    random = thinkRandomNumber(Number(maxOfNumber))
+})
+
+ok.addEventListener("click", () => {
+
+    if (numberOfAttempts == 0) {
+        alertFun (
+            `<strong>You lose!</strong> Siz men oylagan soni topa olmadiz, men oylagan son ${random}`,
+            `alert-danger`,
+            true
+        )
+        display.textContent = ""
+    }else if (display.textContent == random) {
+        alertFun (
+            `<strong>You won!</strong> Siz yutingiz ${count}ta urinishda `,
+            `alert-success`,
+            true
+        )
+        display.textContent = ""
+    }else if (display.textContent > random) {
+        alertFun (
+            `<strong>You have not found!</strong> Sizni kiritgan soniz katta men o'ylagan sondan urinishlar soni ${count}/${numberOfAttemptsValue}ta`,
+            `alert-warning`,
+            false
+        )
+        display.textContent = ""
+    }else if (display.textContent < random) {
+       alertFun (
+            `<strong>You have not found!</strong> Sizni kiritgan soniz kikchik men o'ylagan sondan urinishlar soni ${count}/${numberOfAttemptsValue}ta`,
+            `alert-warning`,
+            false
+       )
+       display.textContent = ""
+    }
+
+    count++
+    numberOfAttempts--
+    display.textContent = ""
+})
